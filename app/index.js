72edb10010c6b47c4b627f4ab09b23235f477fec
Makoto Miyamoto <makotonomeru@gmail.com>
2020-05-19 00:19:08 -0500

    finally\

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.BOT_TOKEN || process.env.TEST_TOKEN;
+const token = process.env.BOT_TOKEN || load(); 
+function load() {
+    require('dotenv').load();
+    return process.env.TEST_TOKEN;
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
