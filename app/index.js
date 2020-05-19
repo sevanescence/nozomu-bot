e030c63763a5bc60e1e0434a488f9383e3514130
Makoto Miyamoto <makotonomeru@gmail.com>
2020-05-19 01:54:44 -0500

    finished development environment

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 // NOTE: prereq worker with "NODE_ENV=production"
 const prod = process.env.NODE_ENV === 'production';
 const token = prod ? process.env.BOT_TOKEN : () => {
@@ -8,14 +9,17 @@ const token = prod ? process.env.BOT_TOKEN : () => {
 };
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag} in ${prod ? 'production':'deployment'} mode.`);
+    console.log(`Logged in as ${client.user.tag} in ${prod ? 'production':'development'} mode.`);
 });
 
 // dm interaction endpoint
 client.on('message', msg => {
     if (msg.channel.type === 'dm' && !msg.author.bot) {
-        console.log(`dm received from ${msg.author.username}#${msg.author.discriminator}`);
+        debug(`dm received from ${msg.author.username}#${msg.author.discriminator}`);
     }
 });
 
-client.login(token);
+client.login(token instanceof Function ? token() : token);
+
+// debug stuff :o
+function debug(s) { if (!prod) console.log(`[DEBUG] ${s}`); }
