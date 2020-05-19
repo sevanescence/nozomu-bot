ddee66430a1a66496256e4cf4eb31d82fff23696
Makoto Miyamoto <makotonomeru@gmail.com>
2020-05-18 23:43:34 -0500

    by god endpoint just work

--- app/index.js
+++ app/index.js
@@ -6,9 +6,9 @@ client.on('ready', msg => {
 });
 
 client.on('message', msg => {
-    if (msg.channel.type === ChannelType.dm) {
-        msg.channel.send("Hey there!" + 
-        "Sorry, I'm gonna say this every time you talk, I'm still in testing phase.");
+    if (msg.channel.type === 'dm') {
+        msg.channel.send('Hey there!' + 
+        'Sorry, I\'m gonna say this every time you talk, I\'m still in testing phase.');
     }
 });
 
