2853e62b7f7e6bc422e182d64a336ebbde8fcf94
Makoto Miyamoto <makotonomeru@gmail.com>
2020-05-19 00:52:04 -0500

    testing environment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: node app/index.js
+worker: NODE_ENV=production node app/index.js
--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.BOT_TOKEN || load(); 
-function load() {
+// NOTE: prereq worker with "NODE_ENV=production"
+const prod = process.env.NODE_ENV === 'production';
+const token = prod ? process.env.BOT_TOKEN : () => {
     require('dotenv').config();
     return process.env.TEST_TOKEN;
-}
+};
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
